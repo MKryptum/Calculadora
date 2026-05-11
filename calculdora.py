81e3c739e8a92bcb729549b16cbfec32afc4ce54
Murilo <murilo.p.soares@senai.edu.br>
2026-05-11 09:12:40 -0300

    Emoção nome e data

--- calculdora.py
+++ calculdora.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Meu Primeiro Programa em Python
-Autor: Murilo
-Data: 11/05/2026
-Descrição: Programa básico para aprender GitHub
-"""
 
 # Importações
 import datetime
@@ -19,10 +13,10 @@ def boas_vindas():
     print("=" * 50)
     print()
 
-    # 1. Olá Mundo básico
+# 1. Olá Mundo básico
     print("1️⃣ Olá, Mundo!")
     print("   Bem-vindo ao GitHub!\n")
-    
+        
     # 2. Informações pessoais
     nome = "[Seu Nome]"
     turma = "[Turma]"
