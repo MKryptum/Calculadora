0a7c0981cba8691b2cbc884c3d750666f45b01f2
Murilo <murilo.p.soares@senai.edu.br>
2026-05-25 08:29:06 -0300

    Alterado soma

--- saudacao.py
+++ saudacao.py
@@ -1,3 +1,9 @@
 def saudacao(nome):
-  return f'Boa tarde , {nome}'
-print(saudacao('Mundo'))
+  return f'Tenha um ótimo final de semana , {nome}'
+
+def soma(n1 , n2):
+  resultado = n1 + n2
+  return(resultado)
+
+
+print(saudacao('Valentina'))
